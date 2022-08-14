a42878bf0cc3f570235cea9f456e9af23392666f
MADHUSRIJ <srimadhu.j@gmail.com>
2022-08-14 19:06:46 +0530

    14 08 2022 07 06

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutheticationComponent } from './authetication/authetication.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'auth', component: AutheticationComponent},
+  {path: '', redirectTo: 'auth', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AutheticationComponent } from './authetication/authetication.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutheticationComponent
   ],
   imports: [
     BrowserModule,
